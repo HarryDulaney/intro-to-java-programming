2c968165a8c1a633628f557935d2a94cf5f05051
H.G.D. IV <harrydulaney@gmail.com>
2019-01-18 14:30:53 -0500

    Add files via upload

---
+++ Exercise01_13.java
@@ -0,0 +1,32 @@
+package Chapter01;
+
+/*(Algebra: solve 2 × 2 linear equations)
+ * Write a program that solves the following 
+ * equation and displays the value for x and y: 
+ *   3.4x+50.2y=44.5
+ *   2.1x+.55y=5.9
+ *  (Using Cramer's rule to solve 2 x 2 linear equations)
+ */
+
+public class Exercise01_13 {
+	public static void main(String[] args) {
+	
+	double a = 3.4;
+	double b = 50.2;
+	double c = 2.1;
+	double d = 0.55;
+	double e = 44.5;
+	double f = 5.9;
+	
+	double x = (e*d - b*f)/ (a*d - b*c);
+	double y = (a*f - e*c)/ (a*d - b*c);
+	
+	System.out.println("The value of x is: " + x );
+	
+	System.out.println("The value of y is: " + y);
+		
+	
+	
+
+	}
+}
