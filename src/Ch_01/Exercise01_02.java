d6a409b8f519454ab572fd6f3ec5af1743b91fbf
H.G.D. IV <20808723+Nothingrhymeswithorange@users.noreply.github.com>
2019-11-07 02:02:56 -0500

    New Solutions

---
+++ src/Ch_01/Exercise01_02.java
@@ -0,0 +1,15 @@
+package Ch_01;
+
+public class Exercise01_02 {
+	public static void main(String[]args) {
+		
+		int count = 0;
+		
+		while(count < 5) {
+			System.out.print("Welcome to Java\n");
+			count++;
+			
+		}
+	}
+
+}
---
+++ src/Ch_01/Exercise01_05.java
@@ -0,0 +1,25 @@
+package Ch_01;
+
+/*
+ * 1.5 (Compute expressions) Write a program that displays the result of
+ 
+9.5 * 4.5 - 2.5 * 3
+45.5 - 3.5
+.
+ */
+
+public class Exercise01_05 {
+	public static void main(String[] args) {
+
+	
+	double numerator = (9.5 * 4.5) - (2.5 * 3);
+	double denominator = 45.5 - 3.5;
+	
+	double solution = numerator/denominator;
+	
+	String outStr = solution + " ";  
+	
+	System.out.printf("%.4f",solution);
+
+	}
+}
