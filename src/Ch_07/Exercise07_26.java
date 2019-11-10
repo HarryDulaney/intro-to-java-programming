f6603ae67a4b83b0bd490dd918d870a514d2d294
H.G.D. IV <20808723+Nothingrhymeswithorange@users.noreply.github.com>
2019-11-09 20:52:25 -0500

    update

--- src/Ch_07/Exercise07_26.java
+++ src/Ch_07/Exercise07_26.java
@@ -5,9 +5,9 @@ package Ch_07;
  *  Write a method that returns true if list1 and list2 are strictly
  *  identical, using the following header:
  *  Write a test program that prompts the user to enter two lists of
- *   integers and displays whether the two are strictly identical.
- *    Here is a sample run. Note that the first number in the input
- *     indicates the number of the elements in the list. 
+ *  integers and displays whether the two are strictly identical.
+ *  Here is a sample run. Note that the first number in the input
+ *  indicates the number of the elements in the list. 
  *  This number is not part of the list.
  */
 
