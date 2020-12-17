0a09b9f80d0cca9adaaa115d27d96464de386d5f
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2020-12-17 08:17:14 -0500

    ch 3 q 11

--- src/ch_03/E0311.java
+++ src/ch_03/E0311.java
@@ -1,5 +1,7 @@
 package ch_03;
 
+import java.util.Scanner;
+
 /*
  * *3.11 (Find the number of days in a month) Write a program that prompts the
  * user to enter the month and year and displays the number of days in the
@@ -10,6 +12,59 @@ package ch_03;
 public class E0311 {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter the month as an integer fro 1 to 12: ");
+		int month = in.nextInt();
+
+		System.out.print("Enter the year: ");
+		int year = in.nextInt();
+		final String commonOutput = "The number of days in the month is ";
+		String days = "";
+
+		switch (month) {
+		case 1:
+			days = "31";
+			break;
+		case 2:
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+				days = "28";
+			} else {
+				days = "29";
+			}
+			break;
+		case 3:
+			days = "31";
+			break;
+		case 4:
+			days = "30";
+			break;
+		case 5:
+			days = "31";
+			break;
+		case 6:
+			days = "30";
+			break;
+		case 7:
+			days = "31";
+			break;
+		case 8:
+			days = "31";
+			break;
+		case 9:
+			days = "30";
+			break;
+		case 10:
+			days = "31";
+			break;
+		case 11:
+			days = "30";
+			break;
+		case 12:
+			days = "31";
+			break;
+		}
+		System.out.println(commonOutput + days);
 
 	}
 
