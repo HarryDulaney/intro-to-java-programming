6e4abf0db610d61a7ba37a4dcd90ae23d254aae3
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-01-24 20:31:47 -0500

    Chapter 7 solutions completed

--- src/ch_08/Exercise08_27.java
+++ src/ch_08/Exercise08_27.java
@@ -8,6 +8,32 @@ import java.util.*;
  * public static double[][] sortColumns(double[][] m)
  */
 public class Exercise08_27 {
-    public static void main(String[] args) {
+
+    public static double[][] sortColumns(double[][] m) {
+        double[][] sorted = new double[m.length][m.length];
+
+        for (int i = 0; i < sorted.length; i++) {
+            for (int j = 0; j < sorted[i].length; j++) {
+                sorted[i][j] = m[i][j];
+            }
+        }
+
+        for (int col = 0; col < sorted.length; col++) {
+            boolean keepSorting = true;
+            while (keepSorting) {
+                keepSorting = false;
+                for (int row = 0; row < sorted[col].length - 1; row++) {
+                    if (sorted[row][col] < sorted[row + 1][col]) {
+                        keepSorting = true;
+                        double temp = sorted[row][col];
+                        sorted[row][col] = sorted[row + 1][col];
+                        sorted[row + 1][col] = temp;
+                    }
+
+                }
+            }
+        }
+        return sorted;
+
     }
 }
