7aa4162ec5096a75203ff5a6458e3be1ad3f22c9
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-01-30 01:27:25 -0500

    Chapter 8 Exercise 25

--- src/ch_08/Exercise08_25.java
+++ src/ch_08/Exercise08_25.java
@@ -7,6 +7,7 @@ import java.util.*;
  * element is positive and the sum of the elements in each column is 1. Write the
  * following method to check whether a matrix is a Markov matrix.
  * public static boolean isMarkovMatrix(double[][] m)
+ * <p>
  * Write a test program that prompts the user to enter a 3 * 3 matrix of double
  * values and tests whether it is a Markov matrix. Here are sample runs:
  * Enter a 3-by-3 matrix row by row:
@@ -19,8 +20,48 @@ import java.util.*;
  * 0.65 0.005 0.225
  * 0.30 0.22 -0.4
  * It is not a Markov matrix
+ *
+ * @author Harry D.
  */
 public class Exercise08_25 {
+    static String markovMatrix = "It is a Markov matrix";
+    static String notMarkovMatrix = "It is not a Markov matrix";
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double[][] test = new double[3][3];
+
+        System.out.println("Enter a 3-by-3 matrix row by row: ");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                test[i][j] = in.nextDouble();
+            }
+        }
+        if (isMarkovMatrix(test)) {
+            System.out.println(markovMatrix);
+        } else {
+            System.out.println(notMarkovMatrix);
+        }
+        in.close();
+
+
+    }
+
+    public static boolean isMarkovMatrix(double[][] m) {
+        for (int i = 0; i < 3; i++) {
+            double sum = 0;
+            for (int j = 0; j < 3; j++) {
+                if (m[j][i] < 0) {
+                    return false;
+                }
+                sum += m[j][i];
+            }
+            if (sum != 1) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
