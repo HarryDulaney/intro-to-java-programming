2946bf5f4ecb3846c1c3a384fe659fe68d2baa8e
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-02-08 21:38:20 -0500

    Chapter Ex 21-04

--- src/ch_21/Exercise21_04.java
+++ src/ch_21/Exercise21_04.java
@@ -2,6 +2,7 @@ package ch_21;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -10,6 +11,7 @@ import java.util.Set;
  * *21.4 (Count consonants and vowels) Write a program that prompts the user to enter a
  * text file name and displays the number of vowels and consonants in the file. Use
  * a set to store the vowels A, E, I, O, and U.
+ * {textfile.txt}
  *
  * @author Harry D.
  */
@@ -20,38 +22,46 @@ public class Exercise21_04 {
     static HashSet<Character> vowels = new HashSet<>();
 
     static {
-        vowels.add('A');
-        vowels.add('E');
-        vowels.add('I');
-        vowels.add('O');
-        vowels.add('U');
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
+
+        Collections.addAll(vowels, 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter path to the text file: ");
-        String filePath = in.next();
-        File file = new File(filePath);
+        System.out.println("Enter the name of a text file to read: ");
+        String fileName = in.nextLine().trim();
+        if (!(new File(fileName).exists())) {
+            fileName = "src" + File.separator + "resources" + File.separator + fileName;
+
+        }
+
+        File file = new File(fileName);
         try {
             Scanner fileScanner = new Scanner(file);
-            while (fileScanner.hasNext()) {
-                String letter = fileScanner.next("[a-z][A-Z]");
-                Character c = letter.charAt(0);
-                if (vowels.contains(c)) {
-                    numVowels++;
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    for (int i = 0; i < word.length(); i++) {
 
-                } else {
-                    numConsonants++;
+                        char character = word.charAt(i);
+                        if (Character.isLetter(character)) {
+                            if (vowels.contains(character)) {
+                                numVowels++;
+                            } else {
+                                numConsonants++;
+                            }
+                        }
+                    }
+
                 }
 
             }
+            fileScanner.close();
         } catch (FileNotFoundException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
+        } finally {
+            in.close();
         }
         System.out.println("Number of vowels = " + numVowels);
         System.out.println("Number of consonants = " + numConsonants);
