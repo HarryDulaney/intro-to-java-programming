634a337222fdbb9ab3fcf0aca239f1facbc3dd00
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-02-10 22:17:18 -0500

    ch 18

--- src/ch_18/Exercise18_08.java
+++ src/ch_18/Exercise18_08.java
@@ -1,14 +1,29 @@
 package ch_18;
 
+import java.util.Scanner;
+
 /**
  * *18.8 (Print the digits in an integer reversely) Write a recursive method that displays
  * an int value reversely on the console using the following header:
+ * <p>
  * public static void reverseDisplay(int value)
  * For example, reverseDisplay(12345) displays 54321. Write a test program
  * that prompts the user to enter an integer and displays its reversal.
  */
 public class Exercise18_08 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an integer to reverse: ");
+        int num = in.nextInt();
+        reverseDisplay(num);
+        in.close();
+    }
+
+    public static void reverseDisplay(int value) {
+        if (value > 0) {
+            System.out.print(value % 10);
+            reverseDisplay(value / 10);
+        }
 
     }
 }
