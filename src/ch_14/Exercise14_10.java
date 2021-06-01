5fddfdf83aff2ca5e37231487dc43b29b1a626a5
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-06-01 06:03:22 -0400

    Chapter 14 Ex's through 11

--- src/ch_14/Exercise14_10.java
+++ src/ch_14/Exercise14_10.java
@@ -1,9 +1,66 @@
 package ch_14;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
 /**
  * 14.10 (Display a cylinder) Write a program that draws a cylinder, as shown in
  * Figure 14.45b. You can use the following method to set the dashed stroke for an arc:
  * arc.getStrokeDashArray().addAll(6.0, 21.0);
  */
-public class Exercise14_10 {
+public class Exercise14_10 extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+
+        Ellipse ellipse = new Ellipse(150, 60, 70, 25);
+        ellipse.setStroke(Color.BLACK);
+        ellipse.setFill(Color.WHITE);
+        pane.getChildren().add(ellipse);
+
+        Arc arc1 = new Arc(ellipse.getCenterX(), 200, ellipse.getRadiusX(),
+                ellipse.getRadiusY(), 0, -180);
+        arc1.setType(ArcType.OPEN);
+        arc1.setFill(Color.WHITE);
+        arc1.setStroke(Color.BLACK);
+        pane.getChildren().add(arc1);
+
+        Arc arc2 = new Arc(ellipse.getCenterX(), arc1.getCenterY(),
+                ellipse.getRadiusX(), ellipse.getRadiusY(), 0, 180);
+        arc2.setType(ArcType.OPEN);
+        arc2.setFill(Color.WHITE);
+        arc2.setStroke(Color.BLACK);
+        arc2.getStrokeDashArray().addAll(6.0, 21.0);
+        pane.getChildren().add(arc2);
+
+
+        Line line1 = new Line(ellipse.getCenterX() - ellipse.getRadiusX(),
+                ellipse.getCenterY(), ellipse.getCenterX() - ellipse.getRadiusX(),
+                arc1.getCenterY());
+        pane.getChildren().add(line1);
+
+        Line line2 = new Line((ellipse.getCenterX() - ellipse.getRadiusX()) +
+                ellipse.getRadiusX() * 2, ellipse.getCenterY(),
+                (ellipse.getCenterX() - ellipse.getRadiusX())
+                        + ellipse.getRadiusX() * 2, arc1.getCenterY());
+
+        pane.getChildren().add(line2);
+
+        Scene scene = new Scene(pane, 300, 250);
+        primaryStage.setTitle(getClass().getName());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
---
+++ src/ch_14/Exercise14_11.java
@@ -0,0 +1,85 @@
+package ch_14;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+
+/**
+ * 14.11 (Paint a smiley face) Write a program that paints a smiley face, as shown in
+ * Figure 14.46a
+ */
+public class Exercise14_11 extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        double faceRadius = 150;
+        Pane pane = new Pane();
+
+        /* Outline of face */
+        Circle circle = new Circle(160, 160, faceRadius);
+        double centerOfFaceX = circle.getCenterX();
+        double centerOfFaceY = circle.getCenterY();
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+        pane.getChildren().add(circle);
+
+        /* Nose */
+        Polygon triangle = new Polygon();
+        triangle.getPoints().addAll(
+                centerOfFaceX, centerOfFaceY + 30,
+                centerOfFaceX - 30, centerOfFaceY - 30,
+                centerOfFaceX + 30, centerOfFaceY - 30
+        );
+        triangle.setRotate(180);
+        triangle.setFill(Color.WHITE);
+        triangle.setStroke(Color.BLACK);
+        pane.getChildren().add(triangle);
+
+        /* Left Eye - Outline */
+        Ellipse ellipse1 = new Ellipse(circle.getCenterX() - 60,circle.getCenterY()-60,circle.getRadius()/6,
+                circle.getRadius()/5);
+        ellipse1.setRotate(90);
+        ellipse1.setFill(Color.TRANSPARENT);
+        ellipse1.setStroke(Color.BLACK);
+        pane.getChildren().add(ellipse1);
+        /* Left Eye - Pupil */
+        Circle circle1 = new Circle(16);
+        circle1.setCenterX(ellipse1.getCenterX());
+        circle1.setCenterY(ellipse1.getCenterY());
+        pane.getChildren().add(circle1);
+
+        /* Right Eye - Outline */
+        Ellipse ellipse2 = new Ellipse(circle.getCenterX() + 60,circle.getCenterY()-60,circle.getRadius()/6,
+                circle.getRadius()/5);
+        ellipse2.setRotate(90);
+        ellipse2.setFill(Color.TRANSPARENT);
+        ellipse2.setStroke(Color.BLACK);
+        pane.getChildren().add(ellipse2);
+        /* Right Eye - Pupil */
+        Circle circle2 = new Circle(16);
+        circle2.setCenterX(ellipse2.getCenterX());
+        circle2.setCenterY(ellipse2.getCenterY());
+        pane.getChildren().add(circle2);
+
+        /* Smile */
+        Arc arc = new Arc(circle.getCenterX(), 200, circle.getRadius()-60,
+                circle.getRadius()-100, 0, -180);
+        arc.setType(ArcType.OPEN);
+        arc.setFill(Color.WHITE);
+        arc.setStroke(Color.BLACK);
+        pane.getChildren().add(arc);
+
+        Scene scene = new Scene(pane, 320, 320);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(getClass().getName());
+        primaryStage.show();
+
+    }
+}
