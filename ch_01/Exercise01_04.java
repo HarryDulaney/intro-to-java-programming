afd1083d7ddb19076f891f7646be1b1ed66487e2
sahil kumar <kumarsahil7753@gmail.com>
2023-08-12 08:38:16 +0530

    updated program for Exercise01_04

--- ch_01/Exercise01_04.java
+++ ch_01/Exercise01_04.java
@@ -11,47 +11,10 @@ package ch_01;
 public class Exercise01_04 {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        int c = 4;
-
-        for (int i = 0; i < 5; i++) {
-            if (i > 0) {
-                System.out.print("\n");
-            }
-            for (int j = 0; j < 3; j++) {
-
-                if (i == 0) {
-                    if (j == 0)
-                        System.out.print("a ");
-                    if (j == 1)
-                        System.out.print("a^2 ");
-                    if (j == 2)
-                        System.out.print("a^3");
-
-                } else if (i == 1) {
-                    System.out.print("1 ");
-                } else if (i == 2) {
-                    System.out.print(a + " ");
-                    a *= 2;
-
-                } else if (i == 3) {
-                    System.out.print(b + " ");
-                    b *= 3;
-
-                } else {
-                    System.out.print(c + " ");
-                    c *= 4;
-
-                }
-
-
-            }
-
-
+        int num = 4;
+        for (int row = 1; row <= num; row++) {
+            System.out.println(row + " " + row * row + " " + row * row * row);
         }
-
     }
-
 
 }
