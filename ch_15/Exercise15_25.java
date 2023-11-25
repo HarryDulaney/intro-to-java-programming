c778dce4ade8406f0733ab092d7427f4297b47f8
Harry Dulaney <harrydulaney@gmail.com>
2023-11-25 11:18:16 -0600

    update readme + Exercise15_25 added

---
+++ ch_15/Exercise15_25.java
@@ -0,0 +1,17 @@
+package ch_15;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * **15.25 (Animation: ball on curve) Write a program that animates a ball moving along
+ * a sine curve, as shown in Figure 15.32. When the ball gets to the right border,
+ * it starts over from the left. Enable the user to resume/pause the animation with
+ * a click on the left/right mouse button.
+ */
+public class Exercise15_25 extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
