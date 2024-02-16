d94227ab3f79f99fb39715585f5f79bd4fa9f4a4
prit1199 <92705386+prit1199@users.noreply.github.com>
2024-02-16 17:43:46 +0530

    Update Exercise06_05.java
    fix logical error that cause incorrect output.

--- ch_06/Exercise06_05.java
+++ ch_06/Exercise06_05.java
@@ -16,15 +16,20 @@ public class Exercise06_05 {
     public static void displaySortedNumbers(double num1, double num2, double num3) {
         double temp;
 
-        if (num1 > num2) {
+         if( num1 < num2 ){
+            temp = num1;
+            num1 = num2;
+            num2 = temp ;
+        }
+        if(num1 < num3){
+            temp = num1 ;
+            num1 = num3;
+            num3 = temp;
+        }
+        if( num2 < num3){
             temp = num2;
-            num2 = num1;
-            num1 = temp;
-        } else if (num2 > num3) {
-            temp = num3;
-            num3 = num2;
-            num2 = temp;
-
+            num2 = num3;
+            num3 = temp;
         }
         System.out.println(num1 + " " + num2 + " " + num3);
     }
